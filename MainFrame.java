b98e33a249de7accb517c0c56a3190704e7b82d6
Carlos <silversycthe@hotmail.com>
2017-05-11 09:00:46 -0700

    splash Screen created

--- MainFrame.java
+++ MainFrame.java
@@ -1,12 +1,19 @@
 import javax.swing.*;
+import java.awt.Toolkit;
+import java.util.concurrent.TimeUnit;
+import java.lang.Exception;
 /**
  * Write a description of class MainFrame here.
  *
  * @author (your name)
  * @version (a version number or a date)
  */
-public class MainFrame extends JFrame
+public class MainFrame extends JDialog
 {
+    private Toolkit tk = Toolkit.getDefaultToolkit();
+    private int screenDim_X = ((int)tk.getScreenSize().getWidth());
+    private int screenDim_Y = ((int)tk.getScreenSize().getHeight());
+
     private Icon splashPic;
     private JLabel splash;
     /**
@@ -14,18 +21,45 @@ public class MainFrame extends JFrame
      */
     public MainFrame()
     {
+        //Frame defaults
         setTitle("Atech Helper");
         setSize(900, 500);
-        
-        splashPic = new ImageIcon(getClass().getResource("maverick.jpg"));
-        splash = new JLabel(splashPic);
-        splash.setBounds(500, 500, 500, 500);
-        
-        
-        
+        setUndecorated(true);
+        setLocation(screenDim_X/2 - 450, screenDim_Y/2-250);
+
+        //Creating splashScreen
+        makeSplash();
         add(splash);
         
         setVisible(true);
+        System.out.println("CHECKING1");
+        
+        delay(3);
+        
+        splash.setVisible(false);
+        setVisible(true);
+        
+        
+        
+        System.out.println("CHECKING3");
     }
-
+    
+    public void delay(int sec)
+    {
+        try
+        {
+            TimeUnit.SECONDS.sleep(sec);
+        }
+        catch(Exception e)
+        {
+        }
+        
+    }
+    
+    public void makeSplash()
+    {
+        splashPic = new ImageIcon(getClass().getResource("maverick_main.png"));
+        splash = new JLabel(splashPic);
+        splash.setBounds(500, 500, 500, 500);
+    }
 }
