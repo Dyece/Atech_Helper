3765272d83208f84f4df654a54fe82c4d23410d8
Carlos <silversycthe@hotmail.com>
2017-05-11 07:33:59 -0700

    Driver and MainFrame class created

---
+++ Driver.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class Driver here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Driver
+{
+    /**
+     * Constructor for objects of class Driver
+     */
+    public static void main(String [] args)
+    {
+        MainFrame myFrame = new MainFrame();
+    }
+}
---
+++ MainFrame.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class MainFrame here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MainFrame
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class MainFrame
+     */
+    public MainFrame()
+    {
+        System.out.println("TEST");
+    }
+
+}
